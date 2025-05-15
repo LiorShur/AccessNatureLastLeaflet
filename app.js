70e35ca56a6383f45c27fd01e4a91e2c4763feae
LiorShur <96306484+LiorShur@users.noreply.github.com>
2025-05-15 15:32:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -303,25 +303,15 @@ function resetApp() {
     map.setView([0, 0], 15);
   }
   // Trigger current location again
-// if (navigator.geolocation) {
-//     watchId = navigator.geolocation.watchPosition(
-//       position => {
-//         const { latitude, longitude, accuracy } = position.coords;
-//         if (accuracy > 25) return;
-  
 if (navigator.geolocation) {
-  watchId = navigator.geolocation.watchPosition(
-      position => {
-        const { latitude, longitude, accuracy } = position.coords;
-        if (accuracy > 25) return;
-  // navigator.geolocation.getCurrentPosition(
-  //   position => {
-  //     const userLocation = {
-  //       lat: position.coords.latitude,
-  //       lng: position.coords.longitude
-      });
-      map.setView(position, 15);
-      marker.setLatLng(position);
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const userLocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setView(userLocation, 17);
+      marker.setLatLng(userLocation);
     },
     error => {
       console.warn("Geolocation failed or denied, using default.");
